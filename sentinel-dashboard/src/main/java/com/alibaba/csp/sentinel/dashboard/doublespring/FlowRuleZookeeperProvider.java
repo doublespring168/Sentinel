b22a1e95017494c4a81c32497f5c665288610810
darcy <>
2019-11-28 18:28:35 +0800

    变更配置文件

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/doublespring/FlowRuleZookeeperProvider.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/doublespring/FlowRuleZookeeperProvider.java
@@ -4,6 +4,11 @@ import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
 import com.alibaba.csp.sentinel.dashboard.rule.DynamicRuleProvider;
 import com.alibaba.csp.sentinel.datasource.Converter;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.api.ExistsBuilder;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -22,9 +27,20 @@ public class FlowRuleZookeeperProvider implements DynamicRuleProvider<List<FlowR
     @Autowired
     private Converter<String, List<FlowRuleEntity>> converter;
 
+    private final Logger logger = LoggerFactory.getLogger(FlowRuleZookeeperProvider.class);
+
+
     @Override
     public List<FlowRuleEntity> getRules(String appName) throws Exception {
-        byte[] data = zkClient.getData().forPath(ZookeeperConfigUtils.getFlowRuleZkPath(appName));
+        String flowRuleZkPath = ZookeeperConfigUtils.getFlowRuleZkPath(appName);
+        ExistsBuilder existsBuilder = zkClient.checkExists();
+        Stat stat = existsBuilder.forPath(flowRuleZkPath);
+        if (stat == null) {
+            logger.info(String.format("即将创建节点[%s]", flowRuleZkPath));
+            zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(flowRuleZkPath, "".getBytes());
+        }
+
+        byte[] data = zkClient.getData().forPath(flowRuleZkPath);
         if (data == null || data.length == 0) {
             return new ArrayList<>();
         }
